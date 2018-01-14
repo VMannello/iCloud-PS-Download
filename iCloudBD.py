5ca13a6ba010925dcbed613c61eed1cf4329eddb
Aarni Koskela <akx@iki.fi>
2018-01-14 13:14:14 +0200

    Add --dump-json and --no-download

--- iCloudBD.py
+++ iCloudBD.py
@@ -45,13 +45,20 @@ def download_items(stream_contents):
                 f.write(r.content)
 
 
-def main():
+def parse_args():
     ap = argparse.ArgumentParser()
     ap.add_argument('url', nargs='?')
+    ap.add_argument('--dump-json', help='dump stream info into this JSON file')
+    ap.add_argument('--no-download', action='store_true', default=False, help='do not download actual items')
     args = ap.parse_args()
     if not args.url:
         print('iCloud Shared Download')
         args.url = input('Paste Entire URL or stream ID: ')
+    return args
+
+
+def main():
+    args = parse_args()
     if '#' in args.url:
         stream_id = args.url.split('#').pop()
     else:
@@ -59,7 +66,14 @@ def main():
     if not stream_id.isalnum():
         raise ValueError('stream ID should be alphanumeric (got %s)' % stream_id)
     stream_contents = get_stream_contents(stream_id)
-    download_items(stream_contents)
+    if args.dump_json:
+        with open(args.dump_json, 'w') as dump_file:
+            json.dump(stream_contents, dump_file, sort_keys=True, indent=2)
+            print('Wrote metadata to %s' % dump_file.name)
+    if not args.no_download:
+        download_items(stream_contents)
+    else:
+        print('Skipping item download (--no-download)')
 
 
 if __name__ == '__main__':
