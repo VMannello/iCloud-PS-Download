28b42c9382ef6764c40288e1fd1462682b58a44e
Aarni Koskela <akx@iki.fi>
2018-01-14 12:59:14 +0200

    Refactor: wrap downloading into a function

--- iCloudBD.py
+++ iCloudBD.py
@@ -2,40 +2,44 @@ import requests
 import json
 import os.path as path
 
-print('iCloud Shared Download')
 
-sharedUrl = input('Paste Entire URL:')
+def download_stream(stream_id):
+    base_url = 'https://p13-sharedstreams.icloud.com/' + stream_id + '/sharedstreams/'
 
-stream_id = sharedUrl.split('#').pop()
-#stream_id = 'B0VJ0DiRHJObc80'
+    url = base_url + 'webstream'
+    print('Getting Photo List From -> ' + url)
 
-base_url = 'https://p13-sharedstreams.icloud.com/' + stream_id + '/sharedstreams/'
-
-url = base_url + 'webstream'
-print('Getting Photo List From -> ' + url)
-
-r = requests.post(url, data='{"streamCtag":null}')
-data = r.json()
-guids = [item['photoGuid'] for item in data['photos']]
-
-chunk = 20
-batches = zip(*[iter(guids)]*chunk)
-
-for batch in batches:
-    url = base_url + 'webasseturls'
-    print('Getting Photo Urls From -> ' + url)
-    r = requests.post(url, data='{"photoGuids":["' + '","'.join(batch) + '"]}')
+    r = requests.post(url, data='{"streamCtag":null}')
     data = r.json()
-    locations = data['locations']
-    items = data['items']
+    guids = [item['photoGuid'] for item in data['photos']]
 
-    for key, item in items.items():
-        file_name = key + '.jpeg'
-        if not path.exists(file_name):
-            location = item['url_location']
-            host = locations[location]['hosts'][0]
-            url = 'https://' + host + item['url_path']
-            print('Downloading Photo -> ' + url)
-            r = requests.get(url)
-            with open(file_name, 'wb') as f:
-                f.write(r.content)
+    chunk = 20
+    batches = zip(*[iter(guids)] * chunk)
+
+    for batch in batches:
+        url = base_url + 'webasseturls'
+        print('Getting Photo Urls From -> ' + url)
+        r = requests.post(url, data='{"photoGuids":["' + '","'.join(batch) + '"]}')
+        data = r.json()
+        locations = data['locations']
+        items = data['items']
+
+        for key, item in items.items():
+            file_name = key + '.jpeg'
+            if not path.exists(file_name):
+                location = item['url_location']
+                host = locations[location]['hosts'][0]
+                url = 'https://' + host + item['url_path']
+                print('Downloading Photo -> ' + url)
+                r = requests.get(url)
+                with open(file_name, 'wb') as f:
+                    f.write(r.content)
+
+
+if __name__ == '__main__':
+    print('iCloud Shared Download')
+    sharedUrl = input('Paste Entire URL:')
+    stream_id = sharedUrl.split('#').pop()
+    download_stream(stream_id)
+
+    # stream_id = 'B0VJ0DiRHJObc80'
